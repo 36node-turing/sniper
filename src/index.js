8eee4e5e7ddae18a1d9d9b067ec840e6fb2d1675
zzswang <zzswang@36node.com>
2021-06-04 13:08:27 +0800

    chore: remove init code

--- src/index.js
+++ src/index.js
@@ -9,17 +9,3 @@ export * from "./lib";
 export * from "./constants";
 
 export { app, config, jobs, services };
-
-/**
- * make sure folder created
- */
-if (!fs.existsSync(config.SCREENSHOTS_PATH)) {
-  fs.mkdirSync(config.SCREENSHOTS_PATH, {
-    recursive: true,
-  });
-}
-if (!fs.existsSync(config.TEMPLATES_PATH)) {
-  fs.mkdirSync(config.TEMPLATES_PATH, {
-    recursive: true,
-  });
-}
